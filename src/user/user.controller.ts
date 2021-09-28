4140f4573348b13e9ed17239381da889d82f9c41
Nazmul Ahmed <grey000906@gmail.com>
2021-09-29 05:43:06 +0600

    User Sign Up validation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,13 @@
-import { Post, Body, Controller } from '@nestjs/common';
+import { Post, Body, Controller, UsePipes } from '@nestjs/common';
 import { UserService } from './user.service';
+import { ValidationPipe } from './help/validation.pipe';
 import { CreateUserDto } from './help/dto';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UsePipes(new ValidationPipe())
   @Post('users')
   async create(@Body('user') userData: CreateUserDto) {
     return this.userService.create(userData);
