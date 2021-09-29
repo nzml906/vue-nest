676c4010cdf5285027debbb9c0dff6d304c839c9
Nazmul Ahmed <grey000906@gmail.com>
2021-09-29 06:20:39 +0600

    implement User Sign in

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,8 @@
 import { Post, Body, Controller, UsePipes } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ValidationPipe } from './help/validation.pipe';
-import { CreateUserDto } from './help/dto';
+import { CreateUserDto, LoginUserDto } from './help/dto';
+import { UserRO } from './help/user.interface';
 
 @Controller()
 export class UserController {
@@ -11,5 +12,11 @@ export class UserController {
   @Post('users')
   async create(@Body('user') userData: CreateUserDto) {
     return this.userService.create(userData);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Post('users/login')
+  async login(@Body('user') loginUserDto: LoginUserDto): Promise<UserRO> {
+    return await this.userService.login(loginUserDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './help/prisma.service';
-import { CreateUserDto } from './help/dto';
+import { CreateUserDto, LoginUserDto } from './help/dto';
 import { UserRO } from './help/user.interface';
 import { HttpException } from '@nestjs/common/exceptions/http.exception';
 import { HttpStatus } from '@nestjs/common';
 import * as argon2 from 'argon2';
+const jwt = require('jsonwebtoken');
+import { SECRET } from './help/config';
 
 const select = {
   email: true,
@@ -41,5 +43,45 @@ export class UserService {
     const user = await this.prisma.user.create({ data, select });
 
     return { user };
+  }
+
+  async login(payload: LoginUserDto): Promise<any> {
+    const _user = await this.prisma.user.findUnique({
+      where: { email: payload.email },
+    });
+
+    const errors = { User: 'email or password wrong' };
+
+    if (!_user) {
+      throw new HttpException({ errors }, 401);
+    }
+
+    const authenticated = await argon2.verify(_user.password, payload.password);
+
+    if (!authenticated) {
+      throw new HttpException({ errors }, 401);
+    }
+
+    const token = await this.generateJWT(_user);
+    const { password, ...user } = _user;
+    return {
+      user: { token, ...user },
+    };
+  }
+
+  public generateJWT(user) {
+    let today = new Date();
+    let exp = new Date(today);
+    exp.setDate(today.getDate() + 60);
+
+    return jwt.sign(
+      {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        exp: exp.getTime() / 1000,
+      },
+      SECRET,
+    );
   }
 }
