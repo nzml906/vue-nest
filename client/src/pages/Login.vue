96de75cd55255ccaddf66d367b058d5ead8b453e
Nazmul Ahmed <grey000906@gmail.com>
2021-09-29 21:11:33 +0600

    login, signup page

---
+++ client/src/pages/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-md row items-start">
+      <q-input v-model="email" filled type="email" hint="Email" />
+      <q-input
+        v-model="password"
+        filled
+        :type="isPwd ? 'password' : 'text'"
+        hint="Password "
+      >
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
+      <q-btn color="secondary" label="Login" />
+    </div>
+  </div>
+</template>
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    return {
+      password: ref(''),
+      isPwd: ref(true),
+      email: ref(''),
+    };
+  },
+};
+</script>
---
+++ client/src/pages/Signup.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-md" style="max-width: 300px">
+      <q-input v-model="text" type="text" hint="Username" />
+      <q-input v-model="email" filled type="email" hint="Email" />
+      <q-input
+        v-model="password"
+        filled
+        :type="isPwd ? 'password' : 'text'"
+        hint="Password "
+      >
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
+      <q-btn color="secondary" label="Sign Up" />
+    </div>
+  </div>
+</template>
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    return {
+      text: ref(''),
+      password: ref(''),
+      isPwd: ref(true),
+      email: ref(''),
+    };
+  },
+};
+</script>
