dde8c1f2b7acbe52b66940fb71c1c7485ad68f2e
Nazmul Ahmed <grey000906@gmail.com>
2021-10-02 10:25:14 +0600

    auth check every routes

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -20,7 +20,9 @@ import routes from './routes';
 export default route<StateInterface>(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory);
+    : process.env.VUE_ROUTER_MODE === 'history'
+    ? createWebHistory
+    : createWebHashHistory;
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
@@ -32,6 +34,16 @@ export default route<StateInterface>(function (/* { store, ssrContext } */) {
     history: createHistory(
       process.env.MODE === 'ssr' ? void 0 : process.env.VUE_ROUTER_BASE
     ),
+  });
+
+  Router.beforeEach((to, from, next) => {
+    const publicPages = ['/login'];
+    const authRequired = !publicPages.includes(to.path);
+    const loggedIn = localStorage.getItem('id_token');
+    if (authRequired && !loggedIn) {
+      return next('/login');
+    }
+    next();
   });
 
   return Router;
